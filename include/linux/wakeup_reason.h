40ed56c7c99a64c823bb7ef39efd5b33ee9f817f
Ruchi Kandoi <kandoiruchi@google.com>
2014-10-29 10:36:27 -0700

    PM: wakeup_reason: add functionality to log the last suspend-abort reason.
    Extends the last_resume_reason to log suspend abort reason. The abort
    reasons will have "Abort:" appended at the start to distinguish itself
    from the resume reason.
    
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    Change-Id: I3207f1844e3d87c706dfc298fb10e1c648814c5f

--- include/linux/wakeup_reason.h
+++ include/linux/wakeup_reason.h
@@ -20,10 +20,13 @@
 
 #include <linux/types.h>
 
+#define MAX_SUSPEND_ABORT_LEN 256
+
 void log_wakeup_reason(int irq);
 #ifdef CONFIG_ARCH_MSM
 void update_wakeup_reason_stats(int irq);
 #endif
+void log_suspend_abort_reason(const char *fmt, ...);
 const int* get_wakeup_reasons(size_t *len);
 void clear_wakeup_reasons(void);
 
