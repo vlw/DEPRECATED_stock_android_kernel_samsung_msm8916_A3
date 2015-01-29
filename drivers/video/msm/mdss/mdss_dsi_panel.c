2c89c71b1fae74d5823b2928df46dfbf1ad2335f
Siddhartha Agrawal <agrawals@codeaurora.org>
2015-01-29 15:59:20 -0800

    msm: mdss: Add dt support to enable clk lane recovery
    This adds dtsi property to enable the clock lane recovery
    mechanism. This will prevent clock hang issues in command
    mode panels.
    - To enable this qcom,dsi-clk-ln-recovery property needs
      to be enabled for that particular target.
    - In case commands need to be sent in HS mode, need to add
      CMD_REQ_HS_MODE to the commands flag list.
    
    Change-Id: I24d61983de9e2d695084de7c0b5eec860c1c1601
    CRs-Fixed: 783621
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>
    Signed-off-by: Bharat Pawar <bpawar@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -225,10 +225,8 @@ void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 	/*Panel ON/Off commands should be sent in DSI Low Power Mode*/
 	if (pcmds->link_state == DSI_LP_MODE)
 		cmdreq.flags  |= CMD_REQ_LP_MODE;
-#if defined(CONFIG_FB_MSM_MDSS_SAMSUNG)
 	else if (pcmds->link_state == DSI_HS_MODE)
-		cmdreq.flags  |= CMD_REQ_HS_MODE;
-#endif
+		cmdreq.flags |= CMD_REQ_HS_MODE;
 
 	cmdreq.rlen = 0;
 	cmdreq.cb = NULL;
